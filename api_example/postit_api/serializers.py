3b5db26f954fc1bf51cfe476444ace98d8e51201
Donatas Noreika <donoras@gmail.com>
2022-10-18 11:06:52 +0300

    1 REST paskaita: įdėtas PostList API rodinys

---
+++ api_example/postit_api/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import (Post,
+                     Comment,
+                     PostLike,
+                     CommentLike)
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'body', 'user', 'created']
+
--- api_example/postit_api/urls.py
+++ api_example/postit_api/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import PostList
 
 urlpatterns = [
+    path('posts', PostList.as_view()),
 ]
