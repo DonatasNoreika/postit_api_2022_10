91683241817ebc60b5baa91fbd0b2304d43f8432
Donatas Noreika <donoras@gmail.com>
2022-10-18 11:26:17 +0300

    1 REST paskaita: įdėtas PostList API rodinio redagavimas, apribotas tik prisijungusiam vartotojui

--- api_example/postit_api/serializers.py
+++ api_example/postit_api/serializers.py
@@ -6,7 +6,10 @@ from .models import (Post,
 
 
 class PostSerializer(serializers.ModelSerializer):
+    user_id = serializers.ReadOnlyField(source='user.id')
+    user = serializers.ReadOnlyField(source='user.username')
+
     class Meta:
         model = Post
-        fields = ['id', 'title', 'body', 'user', 'created']
+        fields = ['id', 'title', 'body', 'user_id', 'user', 'created']
 
